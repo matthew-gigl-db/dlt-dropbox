84bdb2ec5ec92a3e897b53a0adc9070fb174ae24
matthew-gigl-db <matthew-gigl-db>
2024-06-02 19:55:28 +0000

    added dlt-dropbox-silver

--- src/2-dlt-dropbox-silver.py
+++ src/2-dlt-dropbox-silver.py
@@ -1,2 +1,20 @@
 # Databricks notebook source
+import dlt
+
+# COMMAND ----------
+
+import sys, os
+sys.path.append(os.path.abspath(spark.conf.get('bundle.sourcePath')))
+
+import main
+
+# COMMAND ----------
+
+Pipeline = main.IngestionDLT(
+    spark = spark
+    ,volume = spark.conf.get("workflow_inputs.volume_path")
+)
+
+# COMMAND ----------
+
 
