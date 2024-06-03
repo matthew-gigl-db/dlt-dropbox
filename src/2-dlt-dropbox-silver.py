cea126cb9974c9edeec3633b10e42ac1fd1b4154
matthew-gigl-db <matthew-gigl-db>
2024-06-03 16:52:55 +0000

    try with schema

--- src/2-dlt-dropbox-silver.py
+++ src/2-dlt-dropbox-silver.py
@@ -18,7 +18,7 @@ Pipeline = main.IngestionDLT(
 # COMMAND ----------
 
 Pipeline.stage_silver(
-  bronze_table = "allergies_csv_bronze"
+  bronze_table = "mgiglia.synthea.allergies_csv_bronze"
   ,table_name = "allergies"
   ,ddl = 'struct<start:date,stop:date,patient_id:string,encounter_id:string,code:string,system:string,description:string,type:string,category:string,reaction1:string,description1:string,severity1:string,reaction2:string,description2:string,severity2:string>'
 )
