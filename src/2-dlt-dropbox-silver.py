50eceaa84f227a62852a484ea77fb511c59d1de2
matthew-gigl-db <matthew-gigl-db>
2024-06-03 18:07:26 +0000

    set up loop based on reference ddl

--- src/2-dlt-dropbox-silver.py
+++ src/2-dlt-dropbox-silver.py
@@ -17,8 +17,30 @@ Pipeline = main.IngestionDLT(
 
 # COMMAND ----------
 
-Pipeline.stage_silver(
-  bronze_table = "mgiglia.synthea_dev.allergies_csv_bronze"
-  ,table_name = "allergies"
-  ,ddl = 'struct<start:date,stop:date,patient_id:string,encounter_id:string,code:string,system:string,description:string,type:string,category:string,reaction1:string,description1:string,severity1:string,reaction2:string,description2:string,severity2:string>'
-)
+catalog = spark.conf.get("workflow_inputs.catalog")
+schema = spark.conf.get("workflow_inputs.schema")
+
+# COMMAND ----------
+
+ddl_ref = spark.sql(f"select table_name, ddl from {catalog}.{schema}.synthea_silver_schemas").collect()
+ddl_ref = [row.asDict() for row in ddl_ref]
+ddl_ref
+
+# COMMAND ----------
+
+for i in ddl_ref:
+  table_name = i["table_name"]
+  ddl = i["ddl"]
+  Pipeline.stage_silver(
+    bronze_table = f"{catalog}.{schema}.{table_name}_csv_bronze"
+    ,table_name = table_name
+    ,ddl = ddl
+  )
+
+# COMMAND ----------
+
+# Pipeline.stage_silver(
+#   bronze_table = f"{catalog}.{schema}.allergies_csv_bronze"
+#   ,table_name = "allergies"
+#   ,ddl = 'struct<start:date,stop:date,patient_id:string,encounter_id:string,code:string,system:string,description:string,type:string,category:string,reaction1:string,description1:string,severity1:string,reaction2:string,description2:string,severity2:string>'
+# )
